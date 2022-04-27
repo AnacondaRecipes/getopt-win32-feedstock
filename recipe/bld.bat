b9d7eb8b25358c9af5a3fea40872b7d93a99e2c0
leej3 <leej3@quansight.com>
2022-04-27 15:50:51 +0000

    minor fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,6 @@ set getopt_build_arch=x%ARCH%
 msbuild /p:Configuration=%getopt_build_mode%
 if errorlevel 1 exit 1
 
-cp %getopt_build_arch%\%getopt_build_mode%\getopt.dll %LIBRARY_BIN%
-cp %getopt_build_arch%\%getopt_build_mode%\getopt.lib %LIBRARY_LIB%
-cp %SRC_DIR%\getopt.h                                 %LIBRARY_INC%
+copy %getopt_build_arch%\%getopt_build_mode%\getopt.dll %LIBRARY_BIN%
+copy %getopt_build_arch%\%getopt_build_mode%\getopt.lib %LIBRARY_LIB%
+copy %SRC_DIR%\getopt.h                                 %LIBRARY_INC%
